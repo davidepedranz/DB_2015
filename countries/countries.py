eb9110d4ca1178c3d243eb98bec142585281af34
Davide Pedranz <davide.pedranz@gmail.com>
2015-03-24 20:56:03 +0100

    Fix db definition

--- countries/countries.py
+++ countries/countries.py
@@ -26,7 +26,7 @@ engine = create_engine('sqlite:///' + DB_NAME)
 #########################################################################
 
 class Country(Base):
-	__tablename__ = "countries"
+	__tablename__ = "country"
 
 	code = Column(String(2), primary_key = True)
 	name = Column(String(80), nullable = False)
@@ -34,9 +34,9 @@ class Country(Base):
 	area = Column(Integer)
 
 class Population(Base):
-	__tablename__ = "populations"
+	__tablename__ = "population"
 
-	country = Column(String(2), ForeignKey('countries.code'), primary_key = True)
+	country = Column(String(2), ForeignKey('country.code'), primary_key = True)
 	population = Column(Integer)
 	children = Column(Float)
 	adult = Column(Float)
@@ -47,9 +47,9 @@ class Population(Base):
 	country_obj = relationship(Country)
 
 class Economy(Base):
-	__tablename__ = "economies"
+	__tablename__ = "economy"
 
-	country = Column(String(2), ForeignKey('countries.code'), primary_key = True)
+	country = Column(String(2), ForeignKey('country.code'), primary_key = True)
 	gdp = Column(Float)
 	inflation = Column(Float)
 	military = Column(Float)
@@ -58,9 +58,9 @@ class Economy(Base):
 	country_obj = relationship(Country)
 
 class Language(Base):
-	__tablename__ = "languages"
+	__tablename__ = "language"
 
-	code = Column(String(2), ForeignKey('countries.code'), primary_key = True)
+	country = Column(String(2), ForeignKey('country.code'), primary_key = True)
 	language = Column(String(30), primary_key = True)
 	percentage = Column(Float)
 
@@ -91,28 +91,29 @@ countries = [('Belgique', 'BE'), ('Bulgaria', 'BG'), ('Czech Republic', 'CZ'), (
 			 ('Slovenia', 'SI'), ('Slovakia', 'SK'), ('Finland', 'FI'), ('Sweden', 'SE'), ('United Kingdom', 'UK')]
 languages = ['Italian', 'German', 'English', 'Danish', 'Polish', 'French', 'Spanish']
 
-numbers = range(2000)
-percentages = []
-for p in range(1000):
-	percentages.append(p / 10)
+def random_int():
+	return random.randint(0, 5000);
+
+def random_percentage(p=100):
+	return random.randint(0, int(p * 10)) / 10;
 
 # insert data
 for cc in countries:
 	
 	# country
-	c = Country(code=cc[0], name=cc[1], capital=random.choice(numbers), area=random.choice(numbers))
+	c = Country(code=cc[0], name=cc[1], capital=random_int(), area=random_int())
 	session.add(c)
 
 	# population
-	children = random.choice(percentages)
-	adult = 100 - children
-	p = Population(country_obj=c, population=random.choice(numbers), children=children, adult=adult, \
-		birth_rate=random.choice(percentages), death_rate=random.choice(percentages), sex_ratio=random.choice(percentages))
+	children = random_percentage(100)
+	adult = random_percentage(100 - children)
+	p = Population(country_obj=c, population=random_int(), children=children, adult=adult, \
+		birth_rate=random_percentage(), death_rate=random_percentage(), sex_ratio=random_percentage())
 	session.add(p)
 
 	# economy
-	e = Economy(country_obj=c, gdp=random.choice(numbers), inflation=random.choice(percentages), \
-		military=random.choice(percentages), poverty_rate=random.choice(percentages))
+	e = Economy(country_obj=c, gdp=random_int(), inflation=random_percentage(), \
+		military=random_percentage(), poverty_rate=random_percentage())
 	session.add(e)
 
 	# languages
@@ -120,7 +121,7 @@ for cc in countries:
 	for i in range(random.randint(0, MAX_LANGUAGES)):
 		current_lang = random.choice(lang)
 		lang.remove(current_lang)
-		l = Language(country_obj=c, language=current_lang, percentage=random.choice(percentages))
+		l = Language(country_obj=c, language=current_lang, percentage=random_percentage())
 		session.add(l)
 
 # write to db
